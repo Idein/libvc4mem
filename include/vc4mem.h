3eb09e2b5f99ec7b66af4232f6d93b9e10846cbc
Terminus <i.can.speak.c.and.basic@gmail.com>
2017-12-15 17:44:54 +0900

    Use the real vc4mem rather than dmatest

--- include/vc4mem.h
+++ include/vc4mem.h
@@ -2,59 +2,46 @@
 #define VC4MEM_H_
 
 #include <sys/types.h>
-#include <fcntl.h> /* For O_* flags */
-#include <linux/ioctl.h>
-
-    /* From dmatest.h */
-    /* xxx: Need to separate files. */
-
-#ifndef _DMATEST_H
-#define _DMATEST_H
-
-#include <linux/ioctl.h>
-
-struct dmatest_mem_area {
-	unsigned addr;
-	unsigned long size;
-};
-
-#define DMATEST_IOC_MAGIC 'd'
-
-#define DMATEST_IOC_SYNC_FOR_CPU_TO_DEVICE        _IOR( DMATEST_IOC_MAGIC, 0, struct dmatest_mem_area )
-#define DMATEST_IOC_SYNC_FOR_CPU_FROM_DEVICE      _IOR( DMATEST_IOC_MAGIC, 1, struct dmatest_mem_area )
-#define DMATEST_IOC_SYNC_FOR_CPU_BIDIRECTIONAL    _IOR( DMATEST_IOC_MAGIC, 2, struct dmatest_mem_area )
-#define DMATEST_IOC_SYNC_FOR_DEVICE_TO_DEVICE     _IOR( DMATEST_IOC_MAGIC, 3, struct dmatest_mem_area )
-#define DMATEST_IOC_SYNC_FOR_DEVICE_FROM_DEVICE   _IOR( DMATEST_IOC_MAGIC, 4, struct dmatest_mem_area )
-#define DMATEST_IOC_SYNC_FOR_DEVICE_BIDIRECTIONAL _IOR( DMATEST_IOC_MAGIC, 5, struct dmatest_mem_area )
-#define DMATEST_IOC_DMA_ALLOC                     _IOWR(DMATEST_IOC_MAGIC, 6, struct dmatest_mem_area )
-#define DMATEST_IOC_ACCESS_MEM                    _IOWR(DMATEST_IOC_MAGIC, 7, struct dmatest_mem_area )
-#define DMATEST_IOC_INVERT_MEM                    _IOR( DMATEST_IOC_MAGIC, 8, struct dmatest_mem_area )
-
-#endif /* _DMATEST_H */
-
+#include <linux/vc4mem.h>
 
     struct vc4mem_config{
-        char *mmap_dev;
-        int mmap_open_flags;
-        char *vc4mem_dev;
-        int vc4mem_open_flags;
-
+        int is_noncached;
+        char *dev;
         struct vc4mem_private_config *priv;
     };
 
     /* main.c */
+
     void vc4mem_clear_config(struct vc4mem_config *cfg);
     int vc4mem_init(struct vc4mem_config *cfg);
     int vc4mem_finalize(struct vc4mem_config *cfg);
 
     /* map.c */
-    void* vc4mem_map_phys_to_user(const unsigned phys, const size_t size,
-            struct vc4mem_config *cfg);
-    int vc4mem_unmap_phys_to_user(void * const user, const size_t size);
+
     unsigned vc4mem_bus_to_phys(const unsigned bus);
 
-    /* sync.h */
-    int vc4mem_sync_mem(const unsigned long req,
-            const unsigned bus, const size_t size, struct vc4mem_config *cp);
+    void* vc4mem_map_phys_to_user(const unsigned phys, const size_t size,
+            struct vc4mem_config *cfg);
+    int vc4mem_unmap_phys_to_user(void * const user, const size_t size,
+            struct vc4mem_config *cfg);
+
+    void* vc4mem_map_bus_to_user(const unsigned phys, const size_t size,
+            struct vc4mem_config *cfg);
+    int vc4mem_unmap_bus_to_user(void * const user, const size_t size,
+            struct vc4mem_config *cfg);
+
+    /* mem.c */
+
+    void* vc4mem_malloc(const unsigned size, unsigned *busp,
+            struct vc4mem_config *cfg);
+    int vc4mem_malloc_v(struct vc4mem_config *cfg, const unsigned n, ...);
+
+    int vc4mem_free(void * const user, const unsigned bus, const unsigned size,
+            struct vc4mem_config *cfg);
+    int vc4mem_free_v(struct vc4mem_config *cfg, const unsigned n, ...);
+
+    int vc4mem_cpu_cache_op(const vc4mem_cpu_cache_op_t op,
+            const unsigned bus, const unsigned size, struct vc4mem_config *cfg);
+    int vc4mem_cpu_cache_op_v(struct vc4mem_config *cfg, const unsigned n, ...);
 
 #endif /* VC4MEM_H_ */
--- src/map.c
+++ src/map.c
@@ -13,13 +13,18 @@
 #include <errno.h>
 #include <sys/mman.h>
 
+unsigned vc4mem_bus_to_phys(const unsigned bus)
+{
+    return bus & ~0xc0000000;
+}
+
 void* vc4mem_map_phys_to_user(const unsigned phys, const size_t size,
         struct vc4mem_config *cfg)
 {
     void *user;
 
     user = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED,
-            cfg->priv->mmap_fd, phys);
+            cfg->priv->fd, phys);
     if (user == MAP_FAILED) {
         pr_err("mmap: %s\n", strerror(errno));
         user = NULL;
@@ -28,9 +33,12 @@ void* vc4mem_map_phys_to_user(const unsigned phys, const size_t size,
     return user;
 }
 
-int vc4mem_unmap_phys_to_user(void * const user, const size_t size)
+int vc4mem_unmap_phys_to_user(void * const user, const size_t size,
+        struct vc4mem_config *cfg)
 {
     int ret;
+
+    UNUSED(cfg);
 
     ret = munmap(user, size);
     if (ret != 0)
@@ -39,7 +47,14 @@ int vc4mem_unmap_phys_to_user(void * const user, const size_t size)
     return ret;
 }
 
-unsigned vc4mem_bus_to_phys(const unsigned bus)
+void *vc4mem_map_bus_to_user(const unsigned bus, const size_t size,
+        struct vc4mem_config *cfg)
 {
-    return bus & ~0xc0000000;
+    return vc4mem_map_phys_to_user(vc4mem_bus_to_phys(bus), size, cfg);
+}
+
+int vc4mem_unmap_bus_to_user(void * const user, const size_t size,
+        struct vc4mem_config *cfg)
+{
+    return vc4mem_unmap_phys_to_user(user, size, cfg);
 }
